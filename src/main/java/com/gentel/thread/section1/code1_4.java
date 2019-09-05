513c35e19ad9ec27b4bb330c0408ff1f10dd7d59
廖江涛 <liaojt@szfangle.com>
2019-09-05 19:53:58 +0800

    code 1-4

--- src/main/java/com/gentel/thread/section1/code1_4.java
+++ src/main/java/com/gentel/thread/section1/code1_4.java
@@ -1,7 +1,7 @@
 package com.gentel.thread.section1;
 
 import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.BlockingQueue;
 
 /**
  * 工作者线程
@@ -9,11 +9,47 @@ import java.util.concurrent.BlockingDeque;
 public class code1_4 {
 
     public static void main(String[] args) {
+        Helper helper = new Helper();
+        helper.init();
 
+        //此处客户端线程为main线程
+        helper.submit("Something...");
     }
 
-    static class Helper{
-        private final BlockingDeque<String> workQueue = new ArrayBlockingQueue<String>(100);
+    static class Helper {
+        private final BlockingQueue<String> workQueue = new ArrayBlockingQueue<String>(100);
 
+        private final Thread workerThread = new Thread() {
+
+            @Override
+            public void run() {
+                String task = null;
+                while (true) {
+                    try {
+                        // 此处等待获取task
+                        task = workQueue.take();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(doProcess(task));
+                }
+            }
+        };
+
+        public void init() {
+            workerThread.start();
+        }
+
+        protected String doProcess(String task) {
+            return task + "-->processed.";
+        }
+
+        public void submit(String task) {
+            try {
+                workQueue.put(task);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
     }
 }
